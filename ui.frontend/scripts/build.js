61463c74bf9f0e7d4bfd0e9e0fa203b15234839f
salomao-santos <salomao-santos777@gmail.com>
2024-11-07 15:31:01 -0300

    feat: add scripts and files

---
+++ ui.frontend/scripts/build.js
@@ -0,0 +1,18 @@
+
+const proxyquire =  require('proxyquire');
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
+// require env in order to load the .env files before the webpack config
+require('react-scripts/config/env');
+
+// require the original webpack config factory
+const webpackConfigFactory = require('react-scripts/config/webpack.config');
+const webpackConfigPatch = require('./webpack.config');
+
+// delegate to the build.js script and stub the webpack.config import
+proxyquire('react-scripts/scripts/build.js', {
+    '../config/webpack.config': (webpackEnv) => webpackConfigPatch(webpackEnv, webpackConfigFactory(webpackEnv))
+});
